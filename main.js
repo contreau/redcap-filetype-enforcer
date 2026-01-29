119360d8d97388030cf151f0dd3dccc3b0dcc542
contreau <72581161+contreau@users.noreply.github.com>
2026-01-29 14:05:25 -0500

    feat: enforce multiple file types

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-// ? NOTE: This code is for UNIFORMLY applying a single file type restriction to all file upload fields on a REDCap instrument. This currently does not allow for granular control.
+document.addEventListener("DOMContentLoaded", () => {
+  // * EDIT THE CONFIG BEFORE APPLYING THIS CODE TO YOUR PROJECT
+  // - The number of comma-separated strings in this array MUST match the number of file upload fields you have in your project.
+  // - If specifying multiple file types for a single field, the string content itself should be comma-separated.
+  const config = {
+    acceptAttributeValues: [".pdf"],
+  };
 
-window.addEventListener("DOMContentLoaded", () => {
   // * Click event callback
   function setInputAcceptAttribute(filetype) {
     const fileUploadInput = document.querySelector(
@@ -20,10 +25,8 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
     // * Case 2: A file is already uploaded, but the user chooses to either upload a new file (in-place replacement) or delete the current one.
-    // - What's happening: The upload link (a.fileuploadlink) re-renders when a file is uploaded, which removed the click event that we initially gave it.
-    // - So, we observe its parent div for that mutation, and add back the click event assignment to a.fileuploadlink.
-
-    // * Observer logic
+    // - The upload link (a.fileuploadlink) re-renders when a file is uploaded, which removed the click event that we initially assigned it.
+    // - Observe its parent div for that mutation, and add back the click event assignment to a.fileuploadlink.
     const observer = new MutationObserver((mutationList) => {
       for (const mutation of mutationList) {
         if (mutation.type === "childList") {
@@ -34,16 +37,46 @@ window.addEventListener("DOMContentLoaded", () => {
         }
       }
     });
-
-    // * Retrieve the sq_id attribute, which is the Field Name created in the instrument builder. Use it to grab the target container and initiate the observer.
     const fieldName = fileField.getAttribute("sq_id");
     const parentContainerOfUploadButton = fileField.querySelector(`#${fieldName}-linknew`);
     observer.observe(parentContainerOfUploadButton, { childList: true });
   }
 
-  // * Execution: Grab all file field types in the form and apply the above logic. Set the file type as the second argument to registerFileValidation
-  const fileFields = document.querySelectorAll(`tr[fieldtype="file"]`);
-  for (const field of fileFields) {
-    registerFileValidation(field, ".pdf"); // The default file type is set to PDF, but you can replace it with any valid type.
+  // * Execution and error handling
+  try {
+    const fileFields = document.querySelectorAll(`tr[fieldtype="file"]`);
+    const lengthDifference = fileFields.length - config.acceptAttributeValues.length;
+    const absoluteDiff = Math.abs(lengthDifference);
+    const hasSingleFileType = config.acceptAttributeValues.length === 1;
+    if (!Array.isArray(config.acceptAttributeValues)) {
+      throw new Error(
+        `The 'acceptAttributeValues' value in the config must be an array [] of one or more strings. Correct formatting example: [".pdf"]`,
+      );
+    }
+    if (lengthDifference > 0 && !hasSingleFileType) {
+      throw new Error(
+        `The number of strings you provided in the 'acceptAttributeValues' is LESS than the amount of file upload fields in your project. Add ${absoluteDiff} more file type string(s) to the config.`,
+      );
+    } else if (lengthDifference < 0) {
+      console.warn(
+        `The number of strings you provided in 'acceptAttributeValues' exceeds the amount of file upload fields in your project by ${absoluteDiff}. The last ${absoluteDiff} file type(s) in your config will not be applied to any field of your project.`,
+      );
+    } else if (config.acceptAttributeValues.length === 0) {
+      throw new Error(
+        `File types not specified in the config. Specify desired file types as a string within the 'acceptAttributeValues' array (example: [".pdf"]).`,
+      );
+    } else if (hasSingleFileType && config.acceptAttributeValues[0] === "") {
+      throw new Error(
+        `An empty string was passed in 'acceptAttributeValues'. Add a valid file type string to enforce file type uploads.`,
+      );
+    }
+    for (let i = 0; i < fileFields.length; i++) {
+      const filetype = hasSingleFileType
+        ? config.acceptAttributeValues[0]
+        : config.acceptAttributeValues[i];
+      registerFileValidation(fileFields[i], filetype);
+    }
+  } catch (e) {
+    console.error(e);
   }
 });
